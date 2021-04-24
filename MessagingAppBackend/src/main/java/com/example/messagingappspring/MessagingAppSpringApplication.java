b6a90bcfe05813b372968740318cb1f037f02c97
a1367563 <a1367563@univie.ac.at>
2021-04-24 20:09:09 +0200

    Spring project has been created

---
+++ MessagingAppBackend/src/main/java/com/example/messagingappspring/MessagingAppSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.messagingappspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagingAppSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessagingAppSpringApplication.class, args);
+    }
+
+}
---
+++ MessagingAppBackend/src/test/java/com/example/messagingappspring/MessagingAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messagingappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
