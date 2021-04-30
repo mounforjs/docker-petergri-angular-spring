433d3189cafe6b0d449ee52723483ea90171b348
a1367563 <a1367563@univie.ac.at>
2021-04-30 17:33:13 +0200

    Refactoring.

--- MessagingAppFrontend/messagingApp/src/app/app.module.ts
+++ MessagingAppFrontend/messagingApp/src/app/app.module.ts
@@ -4,14 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import {RouterModule} from "@angular/router";
 import { AppComponent } from './app.component';
-import { ChatMenuComponent } from './chat-menu/chat-menu.component';
-import { ChatComponent } from './chat/chat.component';
-import { NewHobbyComponent } from './new-hobby/new-hobby.component';
-import { NewChatRoomComponent } from './new-chat-room/new-chat-room.component';
-import { RegisterFormComponent } from './register-form/register-form.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { AdminValidationComponent } from './admin-validation/admin-validation.component';
-import { MainManuComponent } from './main-manu/main-manu.component';
+import { ChatMenuComponent } from './Component/chat-menu/chat-menu.component';
+import { ChatComponent } from './Component/chat/chat.component';
+import { NewHobbyComponent } from './Component/new-hobby/new-hobby.component';
+import { NewChatRoomComponent } from './Component/new-chat-room/new-chat-room.component';
+import { RegisterFormComponent } from './Component/register-form/register-form.component';
+import { LoginPageComponent } from './Component/login-page/login-page.component';
+import { AdminValidationComponent } from './Component/admin-validation/admin-validation.component';
+import { MainManuComponent } from './Component/main-manu/main-manu.component';
 import { FormsModule }   from '@angular/forms';
 
 @NgModule({
